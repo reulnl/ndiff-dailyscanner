af95b2806f260a188ad5541bd410dd13c706d7f9
reulnl <info@raoulbrouns.nl>
2025-02-01 19:33:07 +0100

    Update scan-ndiff.sh

--- scan-ndiff.sh
+++ scan-ndiff.sh
@@ -15,23 +15,28 @@ cat scan-$date.nmap
 
 # Check for previous scan results
 if [ -e scan-prev.xml ]; then
-    ndiff scan-prev.xml scan-$date.xml > diff-$date
+    ndiff scan-prev.xml scan-$date.xml > diff-$date.txt  # Save with .txt extension
 
     # Check if ndiff output is empty
-    if [ -s diff-$date ]; then
+    if [ -s diff-$date.txt ]; then
         echo "*** NDIFF RESULTS ***"
-        cat diff-$date
+        cat diff-$date.txt
 
         # Check if the output is too long (> 4096 characters)
-        diff_size=$(wc -c < diff-$date)
+        diff_size=$(wc -c < diff-$date.txt)
         if [ "$diff_size" -gt 4000 ]; then
-            # Send as a file
+            # Send message about large file
+            curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
+                 -d "chat_id=$TELEGRAM_CHAT_ID" \
+                 -d "text=*** NDIFF RESULTS ***%0AToo many differences - see attached file."
+
+            # Send as a file with .txt extension
             curl -F "chat_id=$TELEGRAM_CHAT_ID" \
-                 -F "document=@diff-$date" \
+                 -F "document=@diff-$date.txt" \
                  "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument"
         else
             # Send as a message
-            diff_result=$(cat diff-$date)
+            diff_result=$(cat diff-$date.txt)
             curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
                  -d "chat_id=$TELEGRAM_CHAT_ID" \
                  -d "text=*** NDIFF RESULTS ***%0A$diff_result"
@@ -42,7 +47,7 @@ if [ -e scan-prev.xml ]; then
         # Send "No differences found" message to Telegram
         curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
              -d "chat_id=$TELEGRAM_CHAT_ID" \
-             -d "text=*** NDIFF RESULTS *** No differences found."
+             -d "text=*** NDIFF RESULTS ***%0ANo differences found."
     fi
 fi
 
